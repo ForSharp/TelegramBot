90d25ac7209d09edb05a2ed22611a98b7acf5227
Nikita <jetscreamm@gmail.com>
2021-03-08 10:59:38 +0300

    Creating an abstract class InlinePanel

---
+++ TelegramBot/InlinePanels/InlinePanel.cs
@@ -0,0 +1,29 @@
+﻿using Telegram.Bot.Args;
+
+namespace TelegramBot.InlinePanels
+{
+    public abstract class InlinePanel
+    {
+        public void RunCreatingProcess(MessageEventArgs messageEventArgs)
+        {
+            CreateInlinePanel(messageEventArgs);
+            SaveMessageId();
+            DeleteOldPanel();
+        }
+        
+        
+        protected abstract void CreateInlinePanel(MessageEventArgs messageEventArgs);
+
+
+        private void SaveMessageId()
+        {
+            
+        }
+        
+        private void DeleteOldPanel()
+        {
+            
+        }
+
+    }
+}
